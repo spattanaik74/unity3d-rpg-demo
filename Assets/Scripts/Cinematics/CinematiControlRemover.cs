9e5809a5357c75c37abb3824ef261b8220db2d56
Sarthak <spattanaik74@gmail.com>
2020-07-19 14:22:14 +0530

    update

---
+++ Assets/Scripts/Cinematics/CinematiControlRemover.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using RPG.Core;
+using RPG.Control;
+
+namespace RPG.Cinematic
+{
+    public class CinematiControlRemover : MonoBehaviour
+    {
+        GameObject player;
+
+        private void Start() 
+        {
+            GetComponent<PlayableDirector>().played += DisableControl;
+            GetComponent<PlayableDirector>().stopped += EnableControl;
+            player = GameObject.FindWithTag("Player");
+
+
+        }
+
+        void DisableControl(PlayableDirector pd)
+        {
+            player.GetComponent<ActionScheduler>().CancelCurrentAction();
+            player.GetComponent<PlayerMove>().enabled = false;
+        }
+
+        void EnableControl(PlayableDirector pd)
+        {
+            player.GetComponent<PlayerMove>().enabled = true;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematic
+
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        bool alreadyTriggered = false;
+
+        private void OnTriggerEnter(Collider other) 
+        {
+            if(!alreadyTriggered && other.gameObject.tag == "Player")
+            {
+                alreadyTriggered = true;
+                GetComponent<PlayableDirector>().Play();
+            }
+            
+        }
+    }
+
+}
